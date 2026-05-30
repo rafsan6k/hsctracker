aa550caef5fa925c48520def66725501e48730d9
rafsan <rafsan.hasan0108@gmail.com>
2026-05-30 18:45:57 +0600

    update

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,15 @@
 import type { NextConfig } from "next";
+import withPWAInit from "next-pwa";
+
+const withPWA = withPWAInit({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === "development",
+});
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  allowedDevOrigins: ['192.168.0.105', 'localhost:3000'],
+  reactStrictMode: true,
 };
 
-export default nextConfig;
+export default withPWA(nextConfig);
